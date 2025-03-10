0f614562b4bac2fba9e25eded948aeab04cf7737
klabarre <klabarre@dawan.fr>
2025-03-10 13:37:34 +0100

    Push fin de matinée

--- src/test/java/fr/dawan/firstTests/CalculatorTest.java
+++ src/test/java/fr/dawan/firstTests/CalculatorTest.java
@@ -1,20 +1,43 @@
 package fr.dawan.firstTests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Début des test !");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Fin des tests !");
+    }
+
+    @BeforeEach
+    public void setUp(){
+        Calculator calculator = new Calculator();
+        System.out.println("Instanciation d'un nouveau calculator");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        calculator = null;
+        System.out.println("Nettoyage ... ");
+    }
 
     @Test
     void add() {
         // Arrange: préparation des données nécssaires au test
-        Calculator calcultor = new Calculator();
+        //Calculator calcultor = new Calculator();
         int a = 2;
         int b = 3;
 
         // Act: Exécutaion de la méthode à tester
-        int result = calcultor.add(a, b);
+        int result = calculator.add(a, b);
 
         // Assert: Vérif. du résultat
         // assertEquals(5, result);  // si je veux sans commentaires
@@ -24,12 +47,12 @@ class CalculatorTest {
     @Test
     void substract() {
         // Arrange
-        Calculator calcultor = new Calculator();
+        //Calculator calcultor = new Calculator();
         int a = 5;
         int b = 3;
 
         // Act
-        int result = calcultor.substract(5, 3);
+        int result = calculator.substract(5, 3);
 
         // Assert
         assertEquals(2, result, "La soustraction de 5 - 3 devrait retourner 2" );
@@ -38,11 +61,11 @@ class CalculatorTest {
     @Test
     void isPositive() {
         // Arrange
-        Calculator calcultor = new Calculator();
+      //  Calculator calcultor = new Calculator();
         int number = 5;
 
         // Act
-        boolean result = calcultor.isPositive(number);
+        boolean result = calculator.isPositive(number);
 
         // Assert
         assertTrue(result, "Le chiffre 5 devrait être positif");
@@ -51,9 +74,9 @@ class CalculatorTest {
     @Test
     void getMessage() {
         // Arrange
-        Calculator calcultor = new Calculator();
+        //Calculator calcultor = new Calculator();
         // Act
-        String result = calcultor.getMessage();
+        String result = calculator.getMessage();
         // Assert
         assertEquals("Hello, Junit !", result, "Le message doit être 'Hello, Junit !'");
     }
@@ -61,7 +84,7 @@ class CalculatorTest {
     @Test
     void getNullableValue() {
         // Arrange
-        Calculator calculator = new Calculator();
+        //Calculator calculator = new Calculator();
         boolean returnNull = true;
         // Act
         Integer result = calculator.getNullableValue(returnNull);
@@ -73,12 +96,12 @@ class CalculatorTest {
     @Test
     void divide() {
         // Arrange
-        Calculator calcultor = new Calculator();
+//        Calculator calcultor = new Calculator();
         int a = 10;
         int b = 2;
 
         // Act
-        int result = calcultor.divide(a, b);
+        int result = calculator.divide(a, b);
 
         // Assert
         assertEquals(5, result, "la valeur attendue de 10/2 est 5");
@@ -88,11 +111,11 @@ class CalculatorTest {
     @Test
     void squareRoot() {
         // Arrange
-        Calculator calcultor = new Calculator();
+        // Calculator calcultor = new Calculator();
         int number = -1;
 
         // Act et assert
-        assertThrows(IllegalArgumentException.class, () -> calcultor.squareRoot(number), "La méthode devrait " +
+        assertThrows(IllegalArgumentException.class, () -> calculator.squareRoot(number), "La méthode devrait " +
                 "lever une exection");
     }
 
